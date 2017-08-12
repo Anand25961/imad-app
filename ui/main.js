44ca861589d230a9d95c091018c012bf5a2be0b7
Anand25961 <30926294+Anand25961@users.noreply.github.com>
2017-08-12 19:54:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
-    img.style.marginLeft=maginLeft+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function()
 {
