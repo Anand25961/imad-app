8b0c80de0057d77987ea1e20195144a6bc1a1b90
Anand25961 <30926294+Anand25961@users.noreply.github.com>
2017-08-12 18:54:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,39 +7,28 @@ app.use(morgan('combined'));
 
 var articles= {
    var article1:{
-    'title':'Article-one|Anand singh',
+    title:"Article-one|Anand singh",
     heading:"ArticleOne",
-    date:"12 august 2017"
+    date:"12 august 2017",
     content:
-`<p>This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.</p>
+    `<p>This is the content of My First article.</p>`
 
-<p>This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.</p>
-
-<p>This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.This is the content of My First article.
-This is the content of My First article.This is the content of My First article.</p>'
-`
-}; 
+},
     var article2:{
-        title:"Article-two|Anand singh",
+    title:"Article-two|Anand singh",
     heading:"Articletwo",
     date:"13 august 2017",
     content:
-`<p>This is the content of My Second article.</p>'
-`
-    };
+    `<p>This is the content of My Second article.</p>`},
+    
     var article3:{
-        title:"Article-three|Anand singh",
+    title:"Article-three|Anand singh",
     heading:"ArticleThree",
     date:"14 august 2017",
     content:
-`<p>This is the content of My third article.</p>'
-`
-    };
+    `<p>This is the content of third Article</p>`
+    
+    }
 };
 
 function createTemplate (data){
@@ -84,11 +73,11 @@ app.get('/article-1', function (req,res) {
 });
 
 app.get('/article-2', function (req,res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-2.html')); 
+   res.send(createTemplate(article2)); 
 });
 
 app.get('/article-3', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
+    res.send(createTemplate(article3));
 });
 
 app.get('/ui/style.css', function (req, res) {
