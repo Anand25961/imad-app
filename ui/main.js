39df59ba2016eb0eda6204acbd06305a565699d6
Anand25961 <30926294+Anand25961@users.noreply.github.com>
2017-08-18 19:32:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,10 @@ button.onclick=function()
     request.open('GET','http://anandsingh9594.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+//submit nmee
+var nameInput=document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+}
